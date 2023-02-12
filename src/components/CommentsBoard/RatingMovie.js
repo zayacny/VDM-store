66abd58576ed8d31e2527a6f3cae2bc7378ca823
Nick Zaitsev <zaystevnick@gmail.com>
2023-02-12 13:43:12 -0800

    add 10 value rate

--- src/components/CommentsBoard/RatingMovie.js
+++ src/components/CommentsBoard/RatingMovie.js
@@ -4,16 +4,16 @@ import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
 
 const labels = {
-  0.5: 'Useless',
-  1: 'Useless+',
-  1.5: 'Poor',
-  2: 'Poor+',
-  2.5: 'Ok',
-  3: 'Ok+',
-  3.5: 'Good',
-  4: 'Good+',
-  4.5: 'Excellent',
-  5: 'Excellent+',
+  1: 'Useless',
+  2: 'Useless+',
+  3: 'Poor',
+  4: 'Poor+',
+  5: 'Ok',
+  6: 'Ok+',
+  7: 'Good',
+  8: 'Good+',
+  9: 'Excellent',
+  10: 'Excellent+',
 };
 
 function getLabelText(value) {
@@ -34,8 +34,9 @@ export default function HoverRating() {
     >
       <Rating
         name="hover-feedback"
+        max={10}
         value={value}
-        precision={0.5}
+        precision={1}
         getLabelText={getLabelText}
         onChange={(event, newValue) => {
           setValue(newValue);
@@ -43,7 +44,7 @@ export default function HoverRating() {
         onChangeActive={(event, newHover) => {
           setHover(newHover);
         }}
-        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        emptyIcon={<StarIcon fontSize="inherit" />}
       />
       {value !== null && (
         <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
