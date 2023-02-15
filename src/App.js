e7b40ef886919fe6f79e799510de3612cb575902
lixolitr <lixolitr@gmail.com>
2023-02-15 14:59:03 +0200

    feat: add navigation with use of React Router (reactrouter.com)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,74 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Link } from "react-router-dom";
-import MoviePage from "./components/MoviePage";
-// import MoviePag from "./components/Movie";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Movie from "./components/Movie";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* "The Shawshank Redemption" */}
-        </a>
-        {/* <Link to="./MoviePage.js"> Movie Page</Link> */}
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+          <div>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+            <Link to="/movie" className="nav-link">
+              Movie
+            </Link>
+            <br></br>
+          </div>
+        </header>
 
-      {/* <main className="Main-section">
-        <Movie
-          img="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-          name="The Shawshank Redemption"
-          year={1994}
-          rateImdb={9.2}
-          genre="drama"
-          storyLine="Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
-        />
-      </main> */}
-    </div>
+        <main className="App-main">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route
+              path="/movie"
+              element={
+                <Movie
+                  img="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+                  name="The Shawshank Redemption"
+                  year={1994}
+                  rateImdb={9.2}
+                  genre="drama"
+                  storyLine="Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+                />
+              }
+            ></Route>
+          </Routes>
+        </main>
+
+        <footer className="App-footer"></footer>
+      </div>
+    </Router>
   );
 }
 
