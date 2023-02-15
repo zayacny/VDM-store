dcc15757e827b34d142326762e8447139c01f396
lixolitr <lixolitr@gmail.com>
2023-02-15 19:50:34 +0200

    refactor:  change size and position of header (logo) + logo as a link

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MovieList from "./components/MovieList";
+// import MovieList from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Хлопці! Вітаю вас на першому проекті!
+          <img src={logo} className="App-logo" alt="logo" />
         </a>
         <br></br>
-        <div>
-          <MovieList></MovieList>
-        </div>
       </header>
     </div>
   );
