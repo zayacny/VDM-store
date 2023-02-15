0c1f676446d926562575b6c77cbad07fc0cc18c9
lixolitr <lixolitr@gmail.com>
2023-02-15 19:55:29 +0200

    feat:  add Movie component to .App-main sectionk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
 function App() {
   return (
@@ -14,8 +14,18 @@ function App() {
         >
           <img src={logo} className="App-logo" alt="logo" />
         </a>
-        <br></br>
       </header>
+      <main className="App-main">
+        <Movie
+          img="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+          name="The Shawshank Redemption"
+          year={1994}
+          rateImdb={9.2}
+          genre="drama"
+          storyLine="Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+        />
+      </main>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
