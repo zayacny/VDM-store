e8f941e67a1ae6d80237bbaffcb818dfba6024ad
viktor <richevik@gmail.com>
2023-02-17 19:31:51 +0200

    fixed: component have interactive link. way to movie side

--- src/components/FilmsList.js
+++ src/components/FilmsList.js
@@ -1,17 +1,39 @@
 import React from "react";
 import "./FilmsList.css";
 const films = [
-  { name: "titanic", year: 1998 },
-  { name: "black widow", year: 2012 },
-  { name: "sherlok", year: 2005 },
-  { name: "bond", year: 1961 },
-  { name: "turtles", year: 1990 },
+  {
+    name: "titanic",
+    year: 1998,
+    link: "http://baskino.me/films/dramy/585-titanik.html",
+  },
+  {
+    name: "black widow",
+    year: 2012,
+    link: "http://baskino.me/films/boeviki/28579-chernaya-vdova.html",
+  },
+  {
+    name: "sherlok",
+    year: 2005,
+    link: "http://baskino.me/serial/2334-sherlok.html",
+  },
+  {
+    name: "bond",
+    year: 1961,
+    link: "http://baskino.me/films/boeviki/12500-007-spektr.html",
+  },
+  {
+    name: "turtles",
+    year: 1990,
+    link: "http://baskino.me/films/boevie-iskustva/9830-cherepashki-nindzya.html",
+  },
 ];
 
 export default function FilmsList() {
   const listOfFilms = films.map((film) => (
     <li key={film.name}>
-      {film.name} ({film.year})
+      <a href={film.link} className="liStyle">
+        {film.name} ({film.year})
+      </a>
     </li>
   ));
   return <ol className="divStyle">{listOfFilms}</ol>;
