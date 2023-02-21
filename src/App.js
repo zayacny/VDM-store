1c1010bf4c449425c5ce4200de039bc93f1a28f7
viktor <richevik@gmail.com>
2023-02-21 09:51:26 +0200

    fix: props.map is still iside component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import "./App.css";
 import FilmsList from "./components/FilmsList.js";
 
 function App() {
+  const films = [
+    {
+      name: "titanic",
+      year: 1998,
+      link: "http://baskino.me/films/dramy/585-titanik.html",
+      director: "Kendrick Lamar",
+      rating: 8.88,
+    },
+    {
+      name: "black widow",
+      year: 2012,
+      link: "http://baskino.me/films/boeviki/28579-chernaya-vdova.html",
+      director: "J. Lopez",
+      rating: 8.88,
+    },
+    {
+      name: "sherlok",
+      year: 2005,
+      link: "http://baskino.me/serial/2334-sherlok.html",
+      director: "C. Diaz",
+      rating: 8.88,
+    },
+    {
+      name: "bond",
+      year: 1961,
+      link: "http://baskino.me/films/boeviki/12500-007-spektr.html",
+      director: "P. Brosnan",
+      rating: 8.88,
+    },
+    {
+      name: "turtles",
+      year: 1990,
+      link: "http://baskino.me/films/boevie-iskustva/9830-cherepashki-nindzya.html",
+      director: "M. Jackson",
+      rating: 8.88,
+    },
+  ];
+  
   return (
+   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Хлопці! Вітаю вас на першому проекті!
-        </a>
-        <FilmsList></FilmsList>
+        <FilmsList films={films}></FilmsList>
       </header>
     </div>
   );
--- src/components/FilmsList.js
+++ src/components/FilmsList.js
@@ -1,56 +1,25 @@
 import React from "react";
 import "./FilmsList.css";
 
-const films = [
-  {
-    name: "titanic",
-    year: 1998,
-    link: "http://baskino.me/films/dramy/585-titanik.html",
-    director: "Kendrick Lamar",
-    rating: 8.88,
-  },
-  {
-    name: "black widow",
-    year: 2012,
-    link: "http://baskino.me/films/boeviki/28579-chernaya-vdova.html",
-    director: "J. Lopez",
-    rating: 8.88,
-  },
-  {
-    name: "sherlok",
-    year: 2005,
-    link: "http://baskino.me/serial/2334-sherlok.html",
-    director: "C. Diaz",
-    rating: 8.88,
-  },
-  {
-    name: "bond",
-    year: 1961,
-    link: "http://baskino.me/films/boeviki/12500-007-spektr.html",
-    director: "P. Brosnan",
-    rating: 8.88,
-  },
-  {
-    name: "turtles",
-    year: 1990,
-    link: "http://baskino.me/films/boevie-iskustva/9830-cherepashki-nindzya.html",
-    director: "M. Jackson",
-    rating: 8.88,
-  },
-];
-
-export default function FilmsList() {
-  const listOfFilms = films.map((film) => (
+export default function FilmsList(films) {
+  let arr = films.films;
+  const listOfFilms = arr.map((film) => (
     <li key={film.name}>
-      <a href={film.link} className="liStyle">
-        <div className="leftSide">
+      <a href={film.link} className="li-style">
+        <div className="left-side">
           {film.name} ({film.year})
         </div>
-        <div className="rightSide">
+        <div className="right-side">
           {film.director}, IMDb ({film.rating})
         </div>
       </a>
     </li>
   ));
-  return <ul className="divStyle">{listOfFilms}</ul>;
+  return <ul className="div-style">{listOfFilms}</ul>;
 }
+// export default function FilmsList(films) {
+//   console.log(films);
+//   let arr = films.films;
+//   const listOfFilms = arr.map((film) => film);
+//   return console.log(listOfFilms);
+// }
