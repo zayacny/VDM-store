87e0db63e59b4e749238a835555106c0ac51b4f6
viktor <richevik@gmail.com>
2023-02-22 16:33:13 +0200

    bug:featch after response return null/UPDATED

--- src/components/FilmsList.js
+++ src/components/FilmsList.js
@@ -29,25 +29,4 @@ export default function ShowFilmsList() {
   return <ul className="div-style">{listOfFilms}</ul>;
 }
 
-// export default function FilmsList(MyComponent) {
-//   let arr = films.films;
-//   const listOfFilms = arr.map((film) => (
-//     <li key={film.name}>
-//       <a href={film.link} className="li-style">
-//         <div className="left-side">
-//           {film.name} ({film.year})
-//         </div>
-//         <div className="right-side">
-//           {film.director}, IMDb ({film.rating})
-//         </div>
-//       </a>
-//     </li>
-//   ));
-//   return <ul className="div-style">{listOfFilms}</ul>;
-// }
-// export default function FilmsList(films) {
-//   console.log(films);
-//   let arr = films.films;
-//   const listOfFilms = arr.map((film) => film);
-//   return console.log(listOfFilms);
-// }
+
