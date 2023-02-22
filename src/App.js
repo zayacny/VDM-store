71ff9026fe43fbbda150283320521a110cf4d84b
viktor <richevik@gmail.com>
2023-02-22 16:25:09 +0200

    bug:featch after response return null

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import FilmsList from "./components/FilmsList.js";
+
+import ShowFilmsList from "./components/FilmsList";
 
 function App() {
-  const films = [
-    {
-      name: "titanic",
-      year: 1998,
-      link: "http://baskino.me/films/dramy/585-titanik.html",
-      director: "Kendrick Lamar",
-      rating: 8.88,
-    },
-    {
-      name: "black widow",
-      year: 2012,
-      link: "http://baskino.me/films/boeviki/28579-chernaya-vdova.html",
-      director: "J. Lopez",
-      rating: 8.88,
-    },
-    {
-      name: "sherlok",
-      year: 2005,
-      link: "http://baskino.me/serial/2334-sherlok.html",
-      director: "C. Diaz",
-      rating: 8.88,
-    },
-    {
-      name: "bond",
-      year: 1961,
-      link: "http://baskino.me/films/boeviki/12500-007-spektr.html",
-      director: "P. Brosnan",
-      rating: 8.88,
-    },
-    {
-      name: "turtles",
-      year: 1990,
-      link: "http://baskino.me/films/boevie-iskustva/9830-cherepashki-nindzya.html",
-      director: "M. Jackson",
-      rating: 8.88,
-    },
-  ];
-  
   return (
-   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <FilmsList films={films}></FilmsList>
+        <ShowFilmsList></ShowFilmsList>
       </header>
     </div>
   );
--- src/components/FilmsList.js
+++ src/components/FilmsList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./FilmsList.css";
+import { useEffect, useState } from "react";
 
-export default function FilmsList(films) {
-  let arr = films.films;
-  const listOfFilms = arr.map((film) => (
+export default function ShowFilmsList() {
+  const [films, setFilms] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3003/films-list")
+      .then((response) => response.json())
+      .then((data) => {
+        setFilms(data.films);
+      });
+  }, []);
+  console.log(films);
+  
+  const listOfFilms = films.map((film) => (
     <li key={film.name}>
       <a href={film.link} className="li-style">
         <div className="left-side">
@@ -17,6 +28,23 @@ export default function FilmsList(films) {
   ));
   return <ul className="div-style">{listOfFilms}</ul>;
 }
+
+// export default function FilmsList(MyComponent) {
+//   let arr = films.films;
+//   const listOfFilms = arr.map((film) => (
+//     <li key={film.name}>
+//       <a href={film.link} className="li-style">
+//         <div className="left-side">
+//           {film.name} ({film.year})
+//         </div>
+//         <div className="right-side">
+//           {film.director}, IMDb ({film.rating})
+//         </div>
+//       </a>
+//     </li>
+//   ));
+//   return <ul className="div-style">{listOfFilms}</ul>;
+// }
 // export default function FilmsList(films) {
 //   console.log(films);
 //   let arr = films.films;
