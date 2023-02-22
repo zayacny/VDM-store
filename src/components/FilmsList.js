96a2f948d511b2e8338bfdaf8976d4f2e8820729
lixolitr <lixolitr@gmail.com>
2023-02-22 16:52:16 +0200

    fix:  filmList variable

--- src/components/FilmsList.js
+++ src/components/FilmsList.js
@@ -3,18 +3,21 @@ import "./FilmsList.css";
 import { useEffect, useState } from "react";
 
 export default function ShowFilmsList() {
-  const [films, setFilms] = useState(null);
+  const [filmsList, setFilms] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3003/films-list")
       .then((response) => response.json())
-      .then((data) => {
-        setFilms(data.films);
-      });
+      .then((data) => setFilms(data.films));
   }, []);
-  console.log(films);
-  
-  const listOfFilms = films.map((film) => (
+
+  console.log(filmsList);
+
+  if (!filmsList) {
+    return null;
+  }
+
+  const listOfFilms = filmsList.map((film) => (
     <li key={film.name}>
       <a href={film.link} className="li-style">
         <div className="left-side">
@@ -28,5 +31,3 @@ export default function ShowFilmsList() {
   ));
   return <ul className="div-style">{listOfFilms}</ul>;
 }
-
-
